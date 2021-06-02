696019c02a25a303d02a8ade5e2d6da9f7296287
Michal <cizek.michal@gmail.com>
2021-06-01 20:02:17 -0400

    Added comments.

--- main.c
+++ main.c
@@ -5,23 +5,23 @@
 #include <pthread.h>
 static Display *display;
 static bool stop;
-void *listen(){
-  stop = read_pipe();
+void *listen(){//Thread that waits for the stop message.
+  stop = read_pipe(); //Listens on FIFO waiting for the stop message.
   pthread_exit(NULL);
 }
 int main(){
-  if(!pipe_init()){
+  if(!pipe_init()){//If the FIFO has not been created yet.
     stop=false;
     pthread_t th;
-    pthread_create(&th, NULL, listen , NULL);
+    pthread_create(&th, NULL, listen , NULL); //Start listening for the stop message.
     display = XOpenDisplay(NULL);
-    while(!stop){
-      usleep(100000);
+    while(!stop){//While the stop message hasn't been sent.
+      usleep(100000);//Wait 100 ms.
       XTestFakeButtonEvent(display, 1, True, CurrentTime);
       XTestFakeButtonEvent(display, 1, False, CurrentTime);
-      XFlush(display);
+      XFlush(display);//Simulate a button click
     }
-    close_pipe();
+    close_pipe();// Close the FIFO and delete the corresponding file.
     XCloseDisplay(display);
     return 1;
   }
